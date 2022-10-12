e3c117eb8c5b5b2c071bc7d3ef7002b1dd544243
SevdalinM <58071509+SevdalinM@users.noreply.github.com>
2022-10-12 15:57:05 +0100

    worked on class diagram
    + added empty classes

--- Assignment/InvestmentIdeasPlatform/HomePage.Designer.cs
+++ Assignment/InvestmentIdeasPlatform/HomePage.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace InvestmentIdeasPlatform
 {
-    partial class Form1
+    partial class HomePage
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "HomePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
