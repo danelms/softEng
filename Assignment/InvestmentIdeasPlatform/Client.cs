01d0b4cd9dc9ad858b3a4e1d4b530ab5e6891603
danelms <danelms@live.co.uk>
2022-10-17 11:58:34 +0100

    Started progress on classes (Client, User)

--- Assignment/InvestmentIdeasPlatform/Client.cs
+++ Assignment/InvestmentIdeasPlatform/Client.cs
@@ -8,5 +8,16 @@ namespace InvestmentIdeasPlatform
 {
     class Client : User
     {
+        private List<String> preferenceCountry = new List<String>();
+        private List<String> preferenceType = new List<String>();
+        private List<int> preferenceRisk = new List<int>();
+        private List<String> preferenceCurrency = new List<String>();
+        private List<String> preferenceRegion = new List<String>();
+        public Client(String username, String password, byte type) : base(username, password, type)
+        {
+            name = username;
+            pass = password;
+            userType = type;
+        }
     }
 }
--- Assignment/InvestmentIdeasPlatform/User.cs
+++ Assignment/InvestmentIdeasPlatform/User.cs
@@ -8,5 +8,30 @@ namespace InvestmentIdeasPlatform
 {
     abstract class User
     {
+        protected String name;
+        protected String pass;
+        protected byte userType;
+
+        public User(String username, String password, byte type)
+        {
+            name = username;
+            pass = password;
+            userType = type;
+        }
+
+        public String getUserName()
+        {
+            return name;
+        }
+
+        public String getPass()
+        {
+            return pass;
+        }
+
+        public byte getUserType()
+        {
+            return userType;
+        }
     }
 }
