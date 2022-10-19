d9be3ffc11d7a841f3f6c76fef058fbd2b49780f
SevdalinM <58071509+SevdalinM@users.noreply.github.com>
2022-10-19 13:02:38 +0100

    Work done on forms, panel now is scrollable

--- Assignment/InvestmentIdeasPlatform/HomePage.cs
+++ Assignment/InvestmentIdeasPlatform/HomePage.cs
@@ -14,37 +14,48 @@ namespace InvestmentIdeasPlatform
     public partial class HomePage : Form
     {
         Login login = new Login();
+
         public HomePage()
         {
             InitializeComponent();
-            Label test = new Label();
-            test.ForeColor = Color.Black;
+            addPanel();
+            showLogin();
+        }
+
+        private void showLogin() 
+        {
+            login.ShowDialog();
+        }
+        private void addPanel() 
+        {
             Panel thePanel = new Panel();
+            ListBox test = new ListBox();
+            test.Width = 20;
+            test.ForeColor = Color.Black;
+            test.Height = 1000;
+
             thePanel.AutoScroll = true;
-            thePanel.Location = new Point(220,30);
-            thePanel.Size = new Size(800,550);
+            thePanel.Location = new Point(220, 30);
+            thePanel.Size = new Size(800, 550);
             thePanel.BackColor = Color.FromArgb(181, 190, 198);
             thePanel.Visible = true;
-            
-            for (int i = 0; i < 100; i++) 
+
+            for (int i = 0; i < 100; i++)
             {
-                test.Text = i.ToString();
+                test.Items.Add(i);
                 thePanel.Controls.Add(test);
             }
             this.Controls.Add(thePanel);
-            login.Show();
         }
-
         private void exitButton_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Application.Exit();
         }
 
         private void minimiseButton_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
         }
-
 
         private void loginSidebarLabel_Click(object sender, EventArgs e)
         {
@@ -55,7 +66,6 @@ namespace InvestmentIdeasPlatform
         {
             login.Show();
         }
-
 
     }
 }
