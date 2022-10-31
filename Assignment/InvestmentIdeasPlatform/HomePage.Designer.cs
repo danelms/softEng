916598ae0e0d231ff5da74d6b6fc93bc2164fa4a
SevdalinM <sevdalin0223@gmail.com>
2022-10-31 15:54:58 +0000

    home page added content to the main panel

--- Assignment/InvestmentIdeasPlatform/HomePage.Designer.cs
+++ Assignment/InvestmentIdeasPlatform/HomePage.Designer.cs
@@ -42,16 +42,14 @@
             // 
             // sidebarPanel
             // 
-            this.sidebarPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(70)))), ((int)(((byte)(82)))));
             this.sidebarPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.sidebarPanel.Controls.Add(this.homeSidebarButton);
             this.sidebarPanel.Controls.Add(this.loginSidebarButton);
             this.sidebarPanel.Controls.Add(this.usernameLabel);
             this.sidebarPanel.Controls.Add(this.loginSidebarPicturebox);
-            this.sidebarPanel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.sidebarPanel.Location = new System.Drawing.Point(0, 0);
+            this.sidebarPanel.Location = new System.Drawing.Point(0, 30);
             this.sidebarPanel.Name = "sidebarPanel";
-            this.sidebarPanel.Size = new System.Drawing.Size(220, 580);
+            this.sidebarPanel.Size = new System.Drawing.Size(220, 550);
             this.sidebarPanel.TabIndex = 0;
             // 
             // homeSidebarButton
@@ -73,7 +71,7 @@
             this.loginSidebarButton.FlatAppearance.BorderSize = 0;
             this.loginSidebarButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.loginSidebarButton.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginSidebarButton.Location = new System.Drawing.Point(0, 546);
+            this.loginSidebarButton.Location = new System.Drawing.Point(0, 516);
             this.loginSidebarButton.Name = "loginSidebarButton";
             this.loginSidebarButton.Size = new System.Drawing.Size(220, 34);
             this.loginSidebarButton.TabIndex = 5;
@@ -85,7 +83,7 @@
             // 
             this.usernameLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.usernameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(190)))), ((int)(((byte)(198)))));
-            this.usernameLabel.Location = new System.Drawing.Point(0, 121);
+            this.usernameLabel.Location = new System.Drawing.Point(0, 113);
             this.usernameLabel.Name = "usernameLabel";
             this.usernameLabel.Size = new System.Drawing.Size(220, 33);
             this.usernameLabel.TabIndex = 4;
--- Assignment/InvestmentIdeasPlatform/HomePage.cs
+++ Assignment/InvestmentIdeasPlatform/HomePage.cs
@@ -53,13 +53,36 @@ namespace InvestmentIdeasPlatform
             PictureBox picLogo = new PictureBox();
             Bitmap logo = new Bitmap(Properties.Resources.EALogoPnkBackLBFront);
             picLogo.Image = (Image)logo;
-            picLogo.Location = new Point(220,30);
-            picLogo.Size = new Size(200, 200);
-            picLogo.Visible = true;
-            picLogo.BackColor = Color.Black;
-            thePanel.Controls.Add(picLogo);
+            picLogo.Location = new Point(20,20);
+            picLogo.Size = new Size(100, 100);
             picLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            thePanel.Controls.Add(picLogo);
+
+            Label title = new Label();
+            title.Text = "Edwards && Avey Investments";
+            title.Font = new Font("Arial", 32);
+            title.Location = new Point(130, 50);
+            title.ForeColor = Color.Black;
+            title.Width = 600;
+            title.Height = 60;
+            thePanel.Controls.Add(title);
+
+            Panel textContainer = new Panel();
+            Label description = new Label();
+            textContainer.Location = new Point(60, 140);
+            textContainer.Size = new Size(680, 300);
+            textContainer.BackColor = Color.FromArgb(54, 70, 82);
+            description.Text = "Edwards and Avey Investments is your go-to solution for tailored investment bundles. We offer up-to-the-day investment opportunities in groups of assets provided by our highly-experienced partners. These partners collate groups of relevant assets into investment “ideas”. These ideas make it easy for you as an investor to find collections of individual assets that meet your desired investment interests, without the need to personally hand pick them, saving you time and allowing you to grow your investment portfolio much faster, and easier.";
+            description.Location = new Point(20, 35);
+            description.MaximumSize = new Size(640,0);
+            description.AutoSize = true;
+            description.Font = new Font("Arial", 16);
+            textContainer.Controls.Add(description);
+            thePanel.Controls.Add(textContainer);
+
+
             this.Controls.Add(thePanel);
+           
         }
         private void exitButton_Click(object sender, EventArgs e)
         {
