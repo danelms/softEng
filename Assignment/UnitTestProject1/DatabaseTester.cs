1dc544a5020589aba5b0bc50ab29203afa1a96ca
SevdalinM <sevdalin0223@gmail.com>
2022-12-05 10:43:21 +0000

    added 1 more test

--- Assignment/UnitTestProject1/DatabaseTester.cs
+++ Assignment/UnitTestProject1/DatabaseTester.cs
@@ -35,6 +35,31 @@ namespace UnitTestProject1
             cmd.ExecuteNonQuery();
             sqlCon.Close();
         }
-            
+
+        [TestMethod]
+        public void TestSQLiteInsert()
+        {
+            SQLiteConnection sqlCon = new SQLiteConnection("Data Source=testInsertDB.db; Version = 3; New = True; Compress = True; ");
+            sqlCon.Open();
+            SQLiteCommand cmd = sqlCon.CreateCommand();
+            string createTable1 = "CREATE TABLE table_1 (first_column INT, second_column INT)";
+            string createTable2 = "CREATE TABLE table_2 (first_column INT, second_column INT)";
+            string insertFullName = "INSERT INTO table_1([first_column], [second_column]) VALUES ('Sevdalin', 'Marinov') ";
+            string deleteTable1 = "DROP TABLE table_1";
+            string deleteTable2 = "DROP TABLE table_2";
+            cmd.CommandText = createTable1;
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = createTable2;
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = insertFullName;
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = deleteTable1;
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = deleteTable2;
+            cmd.ExecuteNonQuery();
+            sqlCon.Close();
+        }
+
+
     }
 }
